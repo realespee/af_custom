d292b28fa8c7e5b383ded9d968fcb1eccd659573
Hunt16 <vivekthakor1690@gmail.com>
2024-01-25 09:17:59 +0000

    untrack file restore in develop branch

--- afghan_customization/afghan_customization/doctype/awcc_job_completion_certificate/awcc_job_completion_certificate.py
+++ afghan_customization/afghan_customization/doctype/awcc_job_completion_certificate/awcc_job_completion_certificate.py
@@ -2,8 +2,8 @@
 # For license information, please see license.txt
 
 import frappe
-import json
 from frappe.model.document import Document
+import json
 
 class AWCCJobCompletionCertificate(Document):
 	pass
--- afghan_customization/afghan_customization/doctype/make_model_capacity/make_model_capacity.py
+++ afghan_customization/afghan_customization/doctype/make_model_capacity/make_model_capacity.py
@@ -4,5 +4,6 @@
 # import frappe
 from frappe.model.document import Document
 
+
 class MakeModelCapacity(Document):
 	pass
